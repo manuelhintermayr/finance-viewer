ea101fa3921577d35450ec7d70937beb8c488a17
Manuel Hintermayr <manuelhintermayr@users.noreply.github.com>
2018-10-18 14:46:01 +0100

    added basic configuration-file

---
+++ config/configuration.php
@@ -0,0 +1,11 @@
+<?php
+    //Admin credentials:
+    $adminName = "admin";
+    $adminPw = "admin";
+
+    //MySql credentials:
+    $servername = "localhost";
+    $username = "root";
+    $password= "";
+    $dbasename = "FinanceViewer";
+?>
