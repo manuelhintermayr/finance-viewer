14cd3e6e58e155b8fb30779728222e943de90e6b
Manuel Hintermayr <manuelhintermayr@users.noreply.github.com>
2018-10-24 00:47:10 +0100

    implemented first attemp of ajax requests

--- nuxt-finance-viewer/components/Login/LoginForm.vue
+++ nuxt-finance-viewer/components/Login/LoginForm.vue
@@ -4,28 +4,66 @@
     class="container">
     <h1 class="welcome-message">Welcome <span id="messageForTheUser"/></h1>
     <form 
-      class="form" 
-      action="" 
-      method="post">
+      class="form" >
       <input 
         id="user" 
+        v-model="username" 
         type="text" 
         name="username" 
         placeholder="Username">
       <input 
-        id="pass" 
+        id="pass"  
+        v-model="password" 
         type="password" 
         name="password" 
         placeholder="Password">
       <button 
         id="login-button" 
-        type="submit">Login</button>
+        @click.prevent="login">Login</button>
     </form>
+    <ul>
+      <li 
+        v-for="n in news" 
+        :key="n.title">
+        {{ n.title }} - {{ n.url }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    login() {
+      this.$axios
+        .get('http://api.icndb.com/jokes/random/10')
+        .then(response => {
+          // this.jokes = response.data.value
+          console.log('first funzt')
+          //this.$router.replace('/admin')
+        })
+        .catch(error => {
+          console.log('funzt net')
+        })
+
+      let loginData = {
+        user: this.username,
+        pass: this.password
+      }
+
+      this.$axios
+        .post('https://httpbin.org/post', loginData)
+        .then(response => {
+          // this.jokes = response.data.value
+          console.log('first funzt')
+          //this.$router.replace('/admin')
+        })
+        .catch(error => {
+          console.log('funzt net')
+        })
+    }
+  }
+}
 </script>
 
 <style>
