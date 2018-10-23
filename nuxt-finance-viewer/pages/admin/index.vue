9833f742bd7ad68b6d1f13b357e2f9d1528e2d94
Manuel Hintermayr <manuelhintermayr@users.noreply.github.com>
2018-10-23 15:56:58 +0100

    first data binding attempt with vue (based on https://stackoverflow.com/questions/34122255/vue-js-v-model-data-object)

--- nuxt-finance-viewer/pages/admin/index.vue
+++ nuxt-finance-viewer/pages/admin/index.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
     <p>Dashboard for admin user. <a href="..\logout.php">Logout</a></p>
+    <form @submit="addItem">
+      <input 
+        v-model="itemTitle" 
+        type="text">
+      <input 
+        v-model="itemUrl" 
+        type="text">
+      <button type="submit">Add</button>
+    </form>
+
+    <ul>
+      <li 
+        v-for="n in news" 
+        :key="n.title">
+        {{ n.title }} - {{ n.url }}
+      </li>
+    </ul>
     <!-- <bobbles/> -->
   </div>
 </template>
@@ -11,6 +28,23 @@ import Bobbles from '~/components/UI/bobbles'
 export default {
   components: {
     Bobbles
+  },
+  data() {
+    return {
+      news: [
+        {
+          title: 'Test Title',
+          url: '/test-title'
+        }
+      ]
+    }
+  },
+  methods: {
+    addItem: function(e) {
+      e.preventDefault()
+      this.news.push({ title: this.itemTitle, url: this.itemUrl })
+      this.itemTitle = this.itemUrl = ''
+    }
   }
 }
 </script>
