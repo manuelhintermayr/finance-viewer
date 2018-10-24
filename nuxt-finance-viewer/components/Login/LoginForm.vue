dba3abd1f1b84c509e74a52f82c2a3325045385b
Manuel Hintermayr <manuelhintermayr@users.noreply.github.com>
2018-10-24 22:45:26 +0100

    added check for emptySpaces and added errorMessages

--- nuxt-finance-viewer/components/Login/LoginForm.vue
+++ nuxt-finance-viewer/components/Login/LoginForm.vue
@@ -1,36 +1,44 @@
 <template>
-  <div 
-    id="login-container" 
-    class="container">
-    <h1 class="welcome-message">Welcome <span id="messageForTheUser"/></h1>
-    {{ username }}
-    <form 
-      class="form" >
-      <input 
-        id="user" 
-        v-model="username" 
-        type="text" 
-        name="username" 
-        placeholder="Username" 
-        required>
-      <input 
-        id="pass"  
-        v-model="password" 
-        type="password" 
-        name="password" 
-        placeholder="Password" 
-        required>
-      <button 
-        id="login-button" 
-        @click.prevent="login">Login</button>
-    </form>
-    <ul>
-      <li 
-        v-for="n in news" 
-        :key="n.title">
-        {{ n.title }} - {{ n.url }}
-      </li>
-    </ul>
+  <div>
+    <div 
+      id="login-container" 
+      class="container">
+      <h1 class="welcome-message">Welcome <span id="messageForTheUser"/></h1>
+      <form 
+        class="form" >
+        <input 
+          id="user" 
+          v-model="username" 
+          type="text" 
+          name="username" 
+          placeholder="Username" 
+          required>
+        <input 
+          id="pass"  
+          v-model="password" 
+          type="password" 
+          name="password" 
+          placeholder="Password" 
+          required>
+        <button 
+          id="login-button" 
+          @click.prevent="login">Login</button>
+      </form>
+    </div>
+    <div 
+      id="login_error" 
+      class="alert alert-danger closed" 
+      role="alert">
+      <span 
+        class="glyphicon glyphicon-exclamation-sign" 
+        aria-hidden="true"/>
+      <span class="sr-only">Error:</span>
+      <span id="error_message">{{ errorMessage }}</span>
+
+      <span 
+        id="error-close" 
+        class="close">X</span>
+    </div>
   </div>
 </template>
 
@@ -39,7 +47,9 @@ export default {
   data() {
     return {
       username: '',
-      password: ''
+      password: '',
+      errorMessage: 'noError',
+      fullName: ''
     }
   },
   methods: {
@@ -49,17 +59,21 @@ export default {
         password: this.password
       }
 
-      this.$axios
-        .post('http://localhost:64674/login.php', loginData)
-        .then(response => {
-          console.log(response.data)
-          // this.jokes = response.data.value
-          console.log('first funzt')
-          //this.$router.replace('/admin')
-        })
-        .catch(error => {
-          console.log('funzt net')
-        })
+      if (loginData.username == '' || loginData.password == '') {
+        this.errorMessage = 'Please fill out all fields.'
+      } else {
+        this.$axios
+          .post('http://localhost:64674/login.php', loginData)
+          .then(response => {
+            console.log(response.data)
+            // this.jokes = response.data.value
+            console.log('first funzt')
+            //this.$router.replace('/admin')
+          })
+          .catch(error => {
+            console.log('funzt net')
+          })
+      }
     }
   }
 }
