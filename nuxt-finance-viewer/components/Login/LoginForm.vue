efcf34f3ab9da1887a36fc1a2432a02568ebb776
Manuel Hintermayr <manuelhintermayr@users.noreply.github.com>
2018-10-25 01:43:27 +0100

    fixed end animations for logging, moved css, implemented redirecting

--- nuxt-finance-viewer/components/Login/LoginForm.vue
+++ nuxt-finance-viewer/components/Login/LoginForm.vue
@@ -1,42 +1,35 @@
 <template>
-  <div>
+  <div :class="{ formSuccess: loginAnimationGetTextDown, loggingEnd: loginAnimationGetTextUp, fadeEnter: loginIsFinished, fadeEnterActive: loginIsFinished }">
     <div 
       id="login-container" 
       class="container">
       <h1 class="welcome-message">Welcome <span id="messageForTheUser"/>{{ fullName }}</h1>
 
-
-      <button @click="fadeMe">
-        Toggle
-      </button>
       <transition 
-        name="fade" 
-        @enter="enter">
-        <p v-if="show">hello</p>
+        name="fade">
+        <form 
+          v-if="!loginSucceeded"
+          class="form">
+          <input 
+            id="user" 
+            v-model="username" 
+            type="text" 
+            name="username" 
+            placeholder="Username" 
+            required>
+          <input 
+            id="pass"  
+            v-model="password" 
+            type="password" 
+            name="password" 
+            placeholder="Password" 
+            required>
+          <button 
+            id="login-button" 
+            @click.prevent="login">Login</button>
+        </form>
       </transition>
 
-
-      <form 
-        :class="{ closed: loginSucceeded}" 
-        class="form">
-        <input 
-          id="user" 
-          v-model="username" 
-          type="text" 
-          name="username" 
-          placeholder="Username" 
-          required>
-        <input 
-          id="pass"  
-          v-model="password" 
-          type="password" 
-          name="password" 
-          placeholder="Password" 
-          required>
-        <button 
-          id="login-button" 
-          @click.prevent="login">Login</button>
-      </form>
     </div>
     <div 
       id="login_error"
@@ -69,7 +62,9 @@ export default {
       errorMessage: '',
       errorAnimationActivated: false,
       loginSucceeded: false,
-      show: false
+      loginAnimationGetTextDown: false,
+      loginAnimationGetTextUp: false,
+      loginIsFinished: false
     }
   },
   methods: {
@@ -94,6 +89,7 @@ export default {
           })
       }
     },
+
     loginError(contentForErrorMessage) {
       this.errorMessage = contentForErrorMessage
       this.errorAnimationActivated = true
@@ -103,28 +99,27 @@ export default {
         self.errorAnimationActivated = false
       }, 1000)
     },
+
     loginSuccessfull(username, url) {
       var self = this
       setTimeout(function() {
         self.animateUsername(username, 0)
-      }, 1000)
+      }, 1000) //Wait 1 second for transition
       this.errorMessage = ''
       this.loginSucceeded = true
+      this.loginAnimationGetTextDown = true
 
-      // console.log(response.data)
-      //this.$router.replace('/admin')
-      // window.setTimeout(
-      //   'type("#messageForTheUser"," Administrator!",0);',
-      //   1000
-      // )
-      // $('form').fadeOut(500)
-      // $('#login_error').fadeOut(500)
-      // $('body').addClass('form-success')
+      setTimeout(function() {
+        self.loginAnimationGetTextUp = true
+      }, 3000) //Wait 3 Seconds to go up again
+      setTimeout(function() {
+        self.loginIsFinished = true
+      }, 3200) //Disable whole login form
 
-      // window.setTimeout('$("body").addClass("loggingEnd");', 3000)
-      // window.setTimeout('$("#login-container").fadeOut(500);', 3200)
-
-      // window.setTimeout('removeClass("body","form-success");', 4000)
+      setTimeout(function() {
+        window.location = '/' + url
+        // self.$router.replace('/' + url)
+      }, 3300) //Redirect
     },
     animateUsername(newUsername, pos) {
       var cUsername = newUsername.substring(0, pos)
@@ -137,33 +132,123 @@ export default {
           self.animateUsername(newUsername, pos + 1)
         }, 70)
       }
-    },
-    fadeMe: function() {
-      this.show = !this.show
-    },
-
-    enter: function(el, done) {
-      var that = this
-
-      setTimeout(function() {
-        that.show = false
-      }, 3000) // hide the message after 3 seconds
     }
   }
 }
 </script>
 
 <style>
+@charset "UTF-8";
+
+/* Code party shared from:
+ * http://codepen.io/vubon/pen/bdmvbY
+ */
+
+@import url(http://fonts.googleapis.com/css?family=Source+Sans+Pro:200);
+
+.container {
+  max-width: 600px;
+  text-align: center;
+  text-align: -webkit-center;
+  position: absolute;
+  left: 50%;
+  top: 40%;
+  margin: -144px 0px 0px -300px;
+}
+.container h1 {
+  font-size: 40px;
+  -webkit-transition-duration: 1s;
+  transition-duration: 1s;
+  -webkit-transition-timing-function: ease-in-put;
+  transition-timing-function: ease-in-put;
+  font-weight: 200;
+}
+form {
+  padding: 20px 0;
+  position: relative;
+  z-index: 2;
+}
+form input {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  outline: 0;
+  border: 1px solid rgba(255, 255, 255, 0.4);
+  background-color: rgba(255, 255, 255, 0.2);
+  width: 250px;
+  border-radius: 3px;
+  padding: 10px 15px;
+  margin: 0 auto 10px auto;
+  display: block;
+  text-align: center;
+  font-size: 18px;
+  color: white;
+  -webkit-transition-duration: 0.25s;
+  transition-duration: 0.25s;
+  font-weight: 300;
+}
+form input:hover {
+  background-color: rgba(255, 255, 255, 0.4);
+}
+form input:focus {
+  background-color: white;
+  width: 300px;
+  color: #53e3a6;
+}
+form button {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  outline: 0;
+  background-color: white;
+  border: 0;
+  padding: 10px 15px;
+  color: #53e3a6;
+  border-radius: 3px;
+  width: 250px;
+  cursor: pointer;
+  font-size: 18px;
+  -webkit-transition-duration: 0.25s;
+  transition-duration: 0.25s;
+}
+form button:hover {
+  background-color: #f5f7f9;
+}
+
+::-webkit-input-placeholder {
+  color: white;
+}
+:-moz-placeholder {
+  color: white;
+  opacity: 1;
+}
+:-ms-input-placeholder {
+  color: white;
+}
+
+/** Correct Login style */
 .fade-enter-active,
+.fadeEnterActive,
 .fade-leave-active {
-  transition: opacity 1s;
+  transition: opacity 0.5s;
 }
 
 .fade-enter,
+.fadeEnter,
 .fade-leave-to {
   opacity: 0;
 }
 
+.formSuccess .container h1 {
+  -webkit-transform: translateY(85px);
+  transform: translateY(85px);
+}
+.loggingEnd .container h1 {
+  -webkit-transform: translateY(20px);
+  transform: translateY(20px);
+}
+
+/** Error Messages style */
 .close:hover {
   cursor: pointer;
 }
