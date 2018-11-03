f214f9dd7a044be01456d5c3b696544988d1385d
Manuel Hintermayr <manuelhintermayr@users.noreply.github.com>
2018-11-03 22:21:26 +0000

    switched to get all views from the backend

--- dashboard/options.php
+++ dashboard/options.php
@@ -8,7 +8,7 @@
     $username = "root";
     $year = "2018";
 
-    if(!userLoggedIn())
+    if(userLoggedIn())
     {
         header('HTTP/1.1 403 Forbidden');
         echo "Not logged in.";
@@ -42,7 +42,48 @@
 
     function getViews()
     {
+        global $mysqli;
+        global $year;
+        global $username;
+        $sql = "SELECT * FROM `fv_views` WHERE `v_y_year` = '$year' AND `v_u_name` = '$username';";
+        $result = $mysqli->query($sql);
         
+        $resultArray = array();
+        if ($result){
+            while($row = $result->fetch_assoc()) 
+            {
+                $yearData = array(
+                    'january' => $row['v_month_01'],
+                    'february' => $row['v_month_02'],
+                    'march' => $row['v_month_03'],
+                    'april' => $row['v_month_04'],
+                    'may' => $row['v_month_05'],
+                    'june' => $row['v_month_06'],
+                    'july' => $row['v_month_07'],
+                    'august' => $row['v_month_08'],
+                    'september' => $row['v_month_09'],
+                    'october' => $row['v_month_10'],
+                    'november' => $row['v_month_11'],
+                    'december' => $row['v_month_12']
+                );
+
+                $view = array(
+                    'count' => $row['v_id'],
+                    'name' => $row['v_name'],
+                    'description' => $row['v_description'],
+                    'id' => $row['v_html_id'],
+                    'notes' => $row['v_notes'],
+                    'profile_url' => $row['v_profile_url'],
+                    'data' => $yearData
+                );
+                $resultArray[] = $view;
+            }
+            header('Content-Type: application/json');
+            echo json_encode($resultArray, JSON_PRETTY_PRINT);
+        }
+        else{
+            echo '{}';
+        }
     }
 
     function actionNotSupported($action)
--- nuxt-finance-viewer/pages/dashboard/index.vue
+++ nuxt-finance-viewer/pages/dashboard/index.vue
@@ -60,7 +60,7 @@
 
           <span 
             v-for="u in tableViews" 
-            :key="u.id">
+            :key="u.count">
             <input 
               :id="'view-'+u.count" 
               type="radio"
@@ -610,309 +610,64 @@ export default {
       newView_notes: 'This is my local market',
       newView_profile_url:
         'https://cdn.stocksnap.io/img-thumbs/960w/AWJD4WV6W1.jpg',
-      tableViews: [
-        {
-          name: 'FVMarket',
-          description: 'Supermarket',
-          id: 'fvmarket',
-          count: 1,
-          notes: 'This is the FVMarket supermarket. I use it to buy groceries.',
-          viewEnabled: false,
-          profile_url:
-            'https://cdn.stocksnap.io/img-thumbs/960w/AWJD4WV6W1.jpg',
-          data: {
-            january: '1+1+41.69+21.49+43.99+14.29',
-            february: '13.99+52.39+10.79+21.49+23.49',
-            march: '0.45+1.39+2.79+0.2+1.99+2.49+0.99+1.49+0.89',
-            april:
-              '43.54-6+16.55+15.42+3.58+6.86+10.17+17.76+-20+2.47+19.15+4.29+18.3+22.02+7.84+6.98',
-            may: '23.97+2.75+16.39+10.53+18.99+5.34+4.31+5.85+24.34+4.74+12.93',
-            june: '89.4',
-            july: '120.3',
-            august: '153.58',
-            september: '194.01',
-            october: '79.99',
-            november: '112.31',
-            december: '126.29',
-            tempJanuary: '1+1+41.69+21.49+43.99+14.29',
-            tempFebruary: '13.99+52.39+10.79+21.49+23.49',
-            tempMarch: '0.45+1.39+2.79+0.2+1.99+2.49+0.99+1.49+0.89',
-            tempApril:
-              '43.54-6+16.55+15.42+3.58+6.86+10.17+17.76+-20+2.47+19.15+4.29+18.3+22.02+7.84+6.98',
-            tempMay:
-              '23.97+2.75+16.39+10.53+18.99+5.34+4.31+5.85+24.34+4.74+12.93',
-            tempJune: '89.4',
-            tempJuly: '120.3',
-            tempAugust: '153.58',
-            tempSeptember: '194.01',
-            tempOctober: '79.99',
-            tempNovember: '112.31',
-            tempDecember: '126.29'
-          }
-        },
-        {
-          name: 'Richard',
-          description: 'Supermarket',
-          id: 'richard',
-          count: 2,
-          notes:
-            'Richard is like the austrian big supermarket. You can buy practically everything.',
-          viewEnabled: false,
-          profile_url:
-            'https://cdn.stocksnap.io/img-thumbs/960w/R0CTZDRFPY.jpg',
-          data: {
-            january: '1',
-            february: '2',
-            march: '3',
-            april: '4',
-            may: '5',
-            june: '6',
-            july: '7',
-            august: '8',
-            september: '9',
-            october: '10',
-            november: '11',
-            december: '12',
-            tempJanuary: '1',
-            tempFebruary: '2',
-            tempMarch: '3',
-            tempApril: '4',
-            tempMay: '5',
-            tempJune: '6',
-            tempJuly: '7',
-            tempAugust: '8',
-            tempSeptember: '9',
-            tempOctober: '10',
-            tempNovember: '11',
-            tempDecember: '12'
-          }
-        },
-        {
-          name: 'Amazing',
-          description: 'Online E-Commerce',
-          id: 'amazing',
-          count: 3,
-          notes:
-            'Amazing is an online eCommerce. I use it to buy stuff and proteins.',
-          viewEnabled: false,
-          profile_url:
-            'https://image.shutterstock.com/image-photo/boxes-trolley-on-laptop-keyboard-450w-580499569.jpg',
-          data: {
-            january: '123.45',
-            february: '20+35.81+11.04+39.9',
-            march: '30.2',
-            april: '39.1+20+1.30+40.91',
-            may: '88.20',
-            june: '60.19',
-            july: '7.91',
-            august: '88.29',
-            september: '90.69',
-            october: '101.29',
-            november: '119.14',
-            december: '124.17',
-            tempJanuary: '123.45',
-            tempFebruary: '20+35.81+11.04+39.9',
-            tempMarch: '30.2',
-            tempApril: '39.1+20+1.30+40.91',
-            tempMay: '88.20',
-            tempJune: '60.19',
-            tempJuly: '7.91',
-            tempAugust: '88.29',
-            tempSeptember: '90.69',
-            tempOctober: '101.29',
-            tempNovember: '119.14',
-            tempDecember: '124.17'
-          }
-        },
-        {
-          name: 'Food',
-          description: 'Food not included in supermarket',
-          id: 'food',
-          count: 4,
-          notes:
-            'Everything I eat (besides from what I buy at the supermarket) is included in this list.',
-          viewEnabled: false,
-          profile_url:
-            'https://image.shutterstock.com/image-photo/healthy-salad-bowl-quinoa-tomatoes-450w-521741356.jpg',
-          data: {
-            january: '49.25',
-            february: '29.49',
-            march: '90.19',
-            april: '4+1+5+3+3.50+4.5+1+1+1+2+1',
-            may: '5+5+6+7+9',
-            june: '6+4.5',
-            july: '7+70',
-            august: '89.13',
-            september: '99-1',
-            october: '100.01+20.29',
-            november: '111',
-            december: '122',
-            tempJanuary: '49.25',
-            tempFebruary: '29.49',
-            tempMarch: '90.19',
-            tempApril: '4+1+5+3+3.50+4.5+1+1+1+2+1',
-            tempMay: '5+5+6+7+9',
-            tempJune: '6+4.5',
-            tempJuly: '7+70',
-            tempAugust: '89.13',
-            tempSeptember: '99-1',
-            tempOctober: '100.01+20.29',
-            tempNovember: '111',
-            tempDecember: '122'
-          }
-        },
-        {
-          name: 'Car',
-          description: 'Car costs, insurance, gasoline',
-          id: 'car',
-          count: 5,
-          notes:
-            'A car doesn`t come cheap. Everything is spend for this car is included here.',
-          viewEnabled: false,
-          profile_url:
-            'https://image.shutterstock.com/image-photo/creative-background-highbeam-headlight-newest-450w-1191643888.jpg',
-          data: {
-            january: '90+450',
-            february: '80+550',
-            march: '90+350.55',
-            april: '80+154.23',
-            may: '90+431.49',
-            june: '80+350',
-            july: '90+210',
-            august: '80+482',
-            september: '90+211',
-            october: '80+30',
-            november: '91+40',
-            december: '82+430',
-            tempJanuary: '90+450',
-            tempFebruary: '80+550',
-            tempMarch: '90+350.55',
-            tempApril: '80+154.23',
-            tempMay: '90+431.49',
-            tempJune: '80+350',
-            tempJuly: '90+210',
-            tempAugust: '80+482',
-            tempSeptember: '90+211',
-            tempOctober: '80+30',
-            tempNovember: '91+40',
-            tempDecember: '82+430'
-          }
-        },
-        {
-          name: 'Invoices',
-          description: 'Monthly costs, rent, etc.',
-          id: 'invoices',
-          count: 6,
-          notes:
-            'All my invoices, like rent, gas, electricity and else is included in this list.',
-          viewEnabled: false,
-          profile_url:
-            'https://image.shutterstock.com/image-vector/e-commerce-order-450w-1148589737.jpg',
-          data: {
-            january: '750+35+35+25+25',
-            february: '750+35+35+25+30',
-            march: '750+35+35+25+18.78',
-            april: '750+35+35+25+69',
-            may: '750+35+35+25+169',
-            june: '750+35+35+25+385',
-            july: '750+35+35+25+342',
-            august: '750+35+35+25+295',
-            september: '750+9+35+35+25+284',
-            october: '750+10+35+35+25+284',
-            november: '750+112+35+35+25+10',
-            december: '712+35+35+25+130',
-            tempJanuary: '750+35+35+25+25',
-            tempFebruary: '750+35+35+25+30',
-            tempMarch: '750+35+35+25+18.78',
-            tempApril: '750+35+35+25+69',
-            tempMay: '750+35+35+25+169',
-            tempJune: '750+35+35+25+385',
-            tempJuly: '750+35+35+25+342',
-            tempAugust: '750+35+35+25+295',
-            tempSeptember: '750+9+35+35+25+284',
-            tempOctober: '750+10+35+35+25+284',
-            tempNovember: '750+112+35+35+25+10',
-            tempDecember: '712+35+35+25+130'
-          }
-        },
-        {
-          name: 'Phone',
-          description: 'Costs for the phone provider',
-          id: 'phone',
-          count: 7,
-          notes:
-            'A phone doesn`t come cheat. The monthly costs for a phone are included here.',
-          viewEnabled: false,
-          profile_url:
-            'https://image.shutterstock.com/image-photo/hand-holding-black-phone-isolated-450w-386618683.jpg',
-          data: {
-            january: '14.2',
-            february: '14.2',
-            march: '14.2',
-            april: '14.2',
-            may: '14.2+30',
-            june: '14.2',
-            july: '14.2+2.60',
-            august: '8+14.2',
-            september: '19',
-            october: '19',
-            november: '19',
-            december: '19',
-            tempJanuary: '14.2',
-            tempFebruary: '14.2',
-            tempMarch: '14.2',
-            tempApril: '14.2',
-            tempMay: '14.2+30',
-            tempJune: '14.2',
-            tempJuly: '14.2+2.60',
-            tempAugust: '8+14.2',
-            tempSeptember: '19',
-            tempOctober: '19',
-            tempNovember: '19',
-            tempDecember: '19'
-          }
-        },
-        {
-          name: 'Medicine',
-          description: 'Medicine bought from the pharmacy',
-          id: 'medicine',
-          count: 8,
-          notes:
-            'Descriptions and everything I use medically is included here.',
-          viewEnabled: false,
-          profile_url:
-            'https://cdn.stocksnap.io/img-thumbs/960w/LKM1T38B6S.jpg',
-          data: {
-            january: '12+4',
-            february: '2+1.3',
-            march: '3',
-            april: '6+3.3+10.95+3+4.05+8.3',
-            may: '12+5.2+2.2',
-            june: '1+29+4.2',
-            july: '15.4',
-            august: '9.20',
-            september: '9',
-            october: '26.9+29.9+6',
-            november: '12+8.99',
-            december: '13.99+25.28',
-            tempJanuary: '12+4',
-            tempFebruary: '2+1.3',
-            tempMarch: '3',
-            tempApril: '6+3.3+10.95+3+4.05+8.3',
-            tempMay: '12+5.2+2.2',
-            tempJune: '1+29+4.2',
-            tempJuly: '15.4',
-            tempAugust: '9.20',
-            tempSeptember: '9',
-            tempOctober: '26.9+29.9+6',
-            tempNovember: '12+8.99',
-            tempDecember: '13.99+25.28'
-          }
-        }
-      ]
+      tableViews: []
     }
   },
+  mounted() {
+    this.loadAll()
+  },
   methods: {
+    loadAll() {
+      this.$axios
+        .get('dashboard/options.php?action=getViews')
+        .then(response => {
+          let api = response.data
+          api.forEach(element => {
+            this.tableViews.push({
+              name: element.name,
+              description: element.description,
+              id: element.id,
+              count: element.count,
+              notes: element.notes,
+              viewEnabled: false,
+              profile_url: element.profile_url,
+              data: {
+                january: element.data.january,
+                tempJanuary: element.data.january,
+                february: element.data.february,
+                tempFebruary: element.data.february,
+                march: element.data.march,
+                tempMarch: element.data.march,
+                april: element.data.april,
+                tempApril: element.data.april,
+                may: element.data.may,
+                tempMay: element.data.may,
+                june: element.data.june,
+                tempJune: element.data.june,
+                july: element.data.july,
+                tempJuly: element.data.july,
+                august: element.data.august,
+                tempAugust: element.data.august,
+                september: element.data.september,
+                tempSeptember: element.data.september,
+                october: element.data.october,
+                tempOctober: element.data.october,
+                november: element.data.november,
+                tempNovember: element.data.november,
+                december: element.data.december,
+                tempDecember: element.data.december
+              }
+            })
+          })
+          console.log(api)
+        })
+        .catch(error => {
+          console.log(error)
+          alert(
+            'Could not load views. An error calling the middleware occured. Look in the console for further information.'
+          )
+        })
+    },
     disableAll() {
       this.tableViews.forEach(element => {
         element.viewEnabled = false
@@ -921,7 +676,7 @@ export default {
       this.createNewViewEnabled = false
     },
     addView() {
-      this.tableViews.push({
+      let newView = {
         name: this.newView_name,
         description: this.newView_description,
         id: this.newView_id,
@@ -955,7 +710,8 @@ export default {
           december: '0',
           tempDecember: '0'
         }
-      })
+      }
+      this.tableViews.push(newView)
       this.newView_name = this.newView_description = this.newView_id = this.newView_notes = this.newView_profile_url =
         ''
     },
