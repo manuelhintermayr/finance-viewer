bb483401fdc7b00259bb5017a02054d7a65b04df
Manuel Hintermayr <manuel@hintermayr.net>
2019-03-17 23:42:38 +0000

    added to show default page from nuxt financeviewer project

--- FinanceViewerASP.NET/FinanceViewerASP.NET/Startup.cs
+++ FinanceViewerASP.NET/FinanceViewerASP.NET/Startup.cs
@@ -56,7 +56,13 @@ namespace FinanceViewerASP.NET
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Main}/{action=Index}/{id?}");
+                routes.MapRoute(
+                    name: "admins",
+                    template: "{controller=Main}/{action=Index}/Admin"
+                );
+                routes.MapRoute("admin", "admin/{*article}",
+                    defaults: new { controller = "Blog", action = "Article" });
             });
         }
     }
