cfecb38dbc72305acc2a8ae7dd9f7e9cf1a9a16a
Manuel Hintermayr <manuel@hintermayr.net>
2019-04-17 20:29:53 +0100

    added redirecting to main page, when trying to access pages without correct session

--- FinanceViewer.Net/FinanceViewer.Net/Controllers/AdminController.cs
+++ FinanceViewer.Net/FinanceViewer.Net/Controllers/AdminController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FinanceViewer.Net.Models.DbModels;
 
 namespace FinanceViewer.Net.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly financeviewerEntities _context;
+
+        public AdminController()
+        {
+            _context = new financeviewerEntities();
+        }
+
         // GET: Admin
         public ActionResult Admin()
         {
+            if (!_context.UserIsLoggedInCorrectly(Session))
+            {
+                return Redirect("/logout.php");
+            }
+
             return View();
         }
     }
--- FinanceViewer.Net/FinanceViewer.Net/Controllers/DashboardController.cs
+++ FinanceViewer.Net/FinanceViewer.Net/Controllers/DashboardController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FinanceViewer.Net.Models.DbModels;
 
 namespace FinanceViewer.Net.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly financeviewerEntities _context;
+
+        public DashboardController()
+        {
+            _context = new financeviewerEntities();
+        }
+
         // GET: Dashboard
         public ActionResult Dashboard()
         {
+            if (!_context.UserIsLoggedInCorrectly(Session))
+            {
+                return Redirect("/logout.php");
+            }
+
             return View();
         }
     }
