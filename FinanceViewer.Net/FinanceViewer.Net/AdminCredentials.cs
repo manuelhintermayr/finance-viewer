25e052ab1fa5149774cfdb67ca37d0dc78db6f0a
Manuel Hintermayr <manuel@hintermayr.net>
2019-04-17 17:26:18 +0100

    finished implementing database-context from asp.net project

---
+++ FinanceViewer.Net/FinanceViewer.Net/AdminCredentials.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinanceViewer.Net
+{
+    public class AdminCredentials
+    {
+        public static string Username => "admin";
+        public static string Password => "admin";
+    }
+}
--- FinanceViewer.Net/FinanceViewer.Net/Models/DbModels/FinanceViewerDb.Context.cs
+++ FinanceViewer.Net/FinanceViewer.Net/Models/DbModels/FinanceViewerDb.Context.cs
@@ -7,6 +7,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+using System.Linq;
+using System.Web.Helpers;
+using FinanceViewer.Net.Models.GetModels;
+
 namespace FinanceViewer.Net.Models.DbModels
 {
     using System;
@@ -28,5 +32,70 @@ namespace FinanceViewer.Net.Models.DbModels
         public virtual DbSet<fv_users> fv_users { get; set; }
         public virtual DbSet<fv_views> fv_views { get; set; }
         public virtual DbSet<fv_years> fv_years { get; set; }
+
+        public Boolean CheckCorrectUser(LoginData loginData)
+        {
+            if (loginData.username == AdminCredentials.Username && loginData.password == AdminCredentials.Password)
+            {
+                return true;
+            }
+
+            //Check if user was found
+            fv_users finalUser = null;
+            try
+            {
+                finalUser = fv_users.Single(m => m.u_name == loginData.username);
+            }
+            catch (InvalidOperationException) { }
+            if (finalUser == null)
+            {
+                return false;
+            }
+
+            //Check if password is right
+            //string hashedPassword = Crypto.HashPassword(loginData.password);
+            if (Crypto.VerifyHashedPassword(finalUser.u_password, loginData.password))
+            {
+                //Check if username is not blocked
+                if (finalUser.u_isLocked == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public string GetFirstNameForUsername(string username)
+        {
+            if (UserIsAdmin(username))
+            {
+                return "Administrator";
+            }
+
+            //Check if user was found
+            fv_users finalUser = null;
+            try
+            {
+                finalUser = fv_users.Single(m => m.u_name == username);
+            }
+            catch (InvalidOperationException) { }
+            if (finalUser == null)
+            {
+                return "[undefined]";
+            }
+
+            return finalUser.u_firstName;
+        }
+
+        public string GetUrlForUser(string username)
+        {
+            return UserIsAdmin(username) ? "admin" : "dashboard";
+        }
+
+        public bool UserIsAdmin(string username)
+        {
+            return username == AdminCredentials.Username;
+        }
     }
 }
