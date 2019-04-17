546e0c826f3ae0ef8128dab98d613f581a4f4ba3
Manuel Hintermayr <manuel@hintermayr.net>
2019-04-17 16:56:22 +0100

    started to add login controller (not finished)

---
+++ FinanceViewer.Net/FinanceViewer.Net/Controllers/Api/LoginController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinanceViewer.Net.Controllers.Api
+{
+    [Route("/login.php")]
+    public class LoginController : Controller
+    {
+        //// POST: Login/Create
+        //[HttpPost]
+        //public ActionResult Create(FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: Login/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: Login/Edit/5
+        //[HttpPost]
+        //public ActionResult Edit(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add update logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
