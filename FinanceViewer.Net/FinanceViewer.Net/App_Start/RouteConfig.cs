573ac0c6d0b186bee5047753772e91ac5cb8dbdc
Manuel Hintermayr <manuel@hintermayr.net>
2019-04-17 19:09:29 +0100

    fixed custom routing for login controller

--- FinanceViewer.Net/FinanceViewer.Net/App_Start/RouteConfig.cs
+++ FinanceViewer.Net/FinanceViewer.Net/App_Start/RouteConfig.cs
@@ -11,24 +11,24 @@ namespace FinanceViewer.Net
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            //Static Files
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             routes.IgnoreRoute("_nuxt/*");
             routes.IgnoreRoute("favicon.ico");
 
-
+            //Views
             routes.MapRoute(
                 name: "Default",
                 url: "",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-
-            //routes.MapRoute("home", "", defaults: new { controller = "Home", action = "Index" });
             routes.MapRoute("admin", "admin/", defaults: new { controller = "Admin", action = "Admin" });
             routes.MapRoute("dashboard", "dashboard/", defaults: new { controller = "Dashboard", action = "Dashboard" });
 
+            //Api
             routes.MapRoute(
                 "ApiLogin",
-                "login/",
+                "login.php/",
                 new { controller = "Login", action = "post" },
                 new { httpMethod = new HttpMethodConstraint("POST") }
             );
