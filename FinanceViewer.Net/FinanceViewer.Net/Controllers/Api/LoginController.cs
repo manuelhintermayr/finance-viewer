809035eccd9180902d1fdd79f17ec0a01382baac
Manuel Hintermayr <manuel@hintermayr.net>
2019-04-17 17:37:43 +0100

    implemented content from asp.net core project into loginController (not yet functioning or tested, only building without problems)

--- FinanceViewer.Net/FinanceViewer.Net/Controllers/Api/LoginController.cs
+++ FinanceViewer.Net/FinanceViewer.Net/Controllers/Api/LoginController.cs
@@ -1,14 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
+using FinanceViewer.Net.Models.AnswerModels;
+using FinanceViewer.Net.Models.DbModels;
+using FinanceViewer.Net.Models.GetModels;
 
 namespace FinanceViewer.Net.Controllers.Api
 {
-    [Route("/login.php")]
+    [System.Web.Mvc.Route("/login.php")]
     public class LoginController : Controller
     {
+        private readonly financeviewerEntities _context;
+
+        public LoginController()
+        {
+            _context = new financeviewerEntities();
+        }
+
+        [System.Web.Mvc.HttpPost]
+        public async Task<ActionResult> Post([FromBody]LoginData loginData)
+        {
+            //var x = HttpContext.Session.GetString("m_user");
+            //var xyz = _session.GetString("m_user");
+            //var la = HttpContext.Session.Keys;
+
+            if (loginData == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Bad request. Please go to the main page and try again.");
+            }
+
+            if (_context.CheckCorrectUser(loginData))
+            {
+                //HttpContext.Session.SetString("m_user", loginData.username);
+                //HttpContext.Session.SetString("m_password", loginData.password);
+                // ^^ going to update this for security later
+
+                //var asd = HttpContext.Session;
+                //var a = HttpContext.Session.Id;
+                //var f = HttpContext.Session.Keys;
+                //var y = HttpContext.Session.Get("m_user");
+                //var xy = HttpContext.Session.GetString("m_user");
+                //var l = HttpContext.Session.Keys;
+
+                Response.StatusCode = 200;
+
+                return Json(new UserPrefs()
+                {
+                    name = _context.GetFirstNameForUsername(loginData.username),
+                    url = _context.GetUrlForUser(loginData.username)
+                });
+            }
+            else
+            {
+                Response.StatusCode = 400;
+                return Content($"Login invalid {{user: {loginData.username}}}");
+            }
+
+        }
+
+
         //// POST: Login/Create
         //[HttpPost]
         //public ActionResult Create(FormCollection collection)
